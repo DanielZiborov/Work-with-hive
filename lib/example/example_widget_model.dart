a3c86ee9fa92a2dc9dcc2fbc0bdfe49231ed55d1
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 19:43:23 +0300

    TypeAdapter<User> user add

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -4,12 +4,38 @@ import 'package:hive/hive.dart';
 class ExampleWidgetModel {
   ExampleWidgetModel() {
     Hive.initFlutter();
+    Hive.registerAdapter(UserAdapter());
   }
   void doSome() async {
-    var box = await Hive.openBox('testBox');
-    // await box.deleteFromDisk();
-    print(box.keys);
+    var box = await Hive.openBox<User>('testBox');
+    final user = User('Bill', 32);
+    await box.add(user);
     print(box.values);
-    box.close();
+  }
+}
+
+class User {
+  String name;
+  int age;
+  User(this.name, this.age);
+  @override
+  String toString() => 'Name: $name Age: $age';
+}
+
+class UserAdapter extends TypeAdapter<User> {
+  @override
+  User read(BinaryReader reader) {
+    final name = reader.readString();
+    final age = reader.readInt();
+    return User(name, age);
+  }
+
+  @override
+  int get typeId => 0;
+
+  @override
+  void write(BinaryWriter writer, User obj) {
+    writer.writeString(obj.name);
+    writer.writeInt(obj.age);
   }
 }
