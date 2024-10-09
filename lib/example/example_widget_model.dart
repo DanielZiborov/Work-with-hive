e05280f6f8a1309a940f7141adafe7a5505d4ab0
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 18:13:57 +0300

    keys and values

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -6,13 +6,9 @@ class ExampleWidgetModel {
     Hive.initFlutter();
   }
   void doSome() async {
-    var box = await Hive.openBox('test Box');
-    await box.put('friends', ['Bill', 'Alice', 'John']);
-    final list = box.get('friends') as List<String>?;
-    final index = await box.add(['Bill', 'Alice', 'John']);
-    final values = box.getAt(index); 
-    print(index);
-    print(values);
-    await box.close();
+    var box = await Hive.openBox('testBox');
+    print(box.keys);
+    print(box.values);
+    box.close();
   }
 }
