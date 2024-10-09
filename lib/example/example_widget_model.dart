900e847ea853f2541e2f3ff16e505fa2d7a9e93a
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 14:40:36 +0300

    box get

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -1,8 +1,13 @@
 import 'package:hive_flutter/adapters.dart';
+import 'package:hive/hive.dart';
 
 class ExampleWidgetModel {
   ExampleWidgetModel(){
     Hive.initFlutter();
   }
-  void doSome(){}
+  void doSome() async{
+    var box = await Hive.openBox('test Box');
+    final name = box.get('name') as String?;
+    print(name);
+  }
 }
