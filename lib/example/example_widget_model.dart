762f34a9a970c64f774e03dab5ea300a0df4ca5c
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 14:48:37 +0300

    box put

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -2,12 +2,16 @@ import 'package:hive_flutter/adapters.dart';
 import 'package:hive/hive.dart';
 
 class ExampleWidgetModel {
-  ExampleWidgetModel(){
+  ExampleWidgetModel() {
     Hive.initFlutter();
   }
-  void doSome() async{
+  void doSome() async {
     var box = await Hive.openBox('test Box');
+    await box.put('name', 'Bill');
+    await box.put('age', 21);
     final name = box.get('name') as String?;
     print(name);
+    final age = box.get('age') as int?;
+    print(age);
   }
 }
