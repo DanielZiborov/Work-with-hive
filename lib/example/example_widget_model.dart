cf1186f923f9118d438fea85dc76f74e9aa42865
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-10 18:53:00 +0300

    TypeAdapterGenerator

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -1,40 +1,31 @@
 import 'package:hive_flutter/adapters.dart';
 import 'package:hive/hive.dart';
 
+part 'example_widget_model.g.dart';
+
 class ExampleWidgetModel {
   void doSome() async {
     if (!Hive.isAdapterRegistered(0)) {
       Hive.registerAdapter(UserAdapter());
     }
+
     var box = await Hive.openBox<User>('testBox');
-    final user = box.getAt(0);
+    final index = await box.add(User('Daniel',1000));
+    final user = await box.getAt(0);
     print(user);
+    print(box.keys);
+    print(box.values);
     box.close();
   }
 }
 
+@HiveType(typeId: 0)
 class User {
+  @HiveField(0)
   String name;
+  @HiveField(1)
   int age;
   User(this.name, this.age);
   @override
   String toString() => 'Name: $name Age: $age';
-}
-
-class UserAdapter extends TypeAdapter<User> {
-  @override
-  User read(BinaryReader reader) {
-    final name = reader.readString();
-    final age = reader.readInt();
-    return User(name, age);
-  }
-
-  @override
-  int get typeId => 0;
-
-  @override
-  void write(BinaryWriter writer, User obj) {
-    writer.writeString(obj.name);
-    writer.writeInt(obj.age);
-  }
 }
