007d8effd9ae188bb7745517a71a272934d90fa6
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-11 23:14:33 +0300

    final first part

--- lib/example/example_widget_model.dart
+++ lib/example/example_widget_model.dart
@@ -9,17 +9,26 @@ class ExampleWidgetModel {
       Hive.registerAdapter(UserAdapter());
     }
 
+    if (!Hive.isAdapterRegistered(1)) {
+      Hive.registerAdapter(PetAdapter());
+    }
+
     var box = await Hive.openBox<User>('testBox');
-    // final index = await box.add(User('Daniel', 1000, 'Zib'));
-    // final user = await box.getAt(index);
-    // print(user);
-    // print(box.keys);
+    var petBox = await Hive.openBox<Pet>('petBox');
+
+    // final pet = Pet('Alex');
+    // await petBox.add(pet);
+    // final pets = HiveList(petBox, objects: [pet]);
+    // final user = User('Ivan', 54, pets);
+    // await box.put('user',user);
     // print(box.values);
 
-    final user = box.get('Ivan');
-    await user?.delete();
-    print(box.get('Ivan'));
+    final user = box.get('user');
+
+    print(user);
+
     box.close();
+    petBox.close();
   }
 }
 
@@ -29,12 +38,14 @@ class User extends HiveObject {
   String name;
   @HiveField(1)
   int age;
-  User(this.name, this.age);
+  @HiveField(3)
+  HiveList<Pet>? pets;
+  User(this.name, this.age, this.pets);
   @override
-  String toString() => 'Name: $name Age: $age';
+  String toString() => 'Name: $name Age: $age Pets: ${pets?.toList()}';
 }
 
-@HiveType(typeId: 0)
+@HiveType(typeId: 1)
 class Pet extends HiveObject {
   @HiveField(0)
   String name;
--- lib/example/example_widget_model.g.dart
+++ lib/example/example_widget_model.g.dart
@@ -19,17 +19,20 @@ class UserAdapter extends TypeAdapter<User> {
     return User(
       fields[0] as String,
       fields[1] as int,
+      (fields[3] as HiveList?)?.castHiveList(),
     );
   }
 
   @override
   void write(BinaryWriter writer, User obj) {
     writer
-      ..writeByte(2)
+      ..writeByte(3)
       ..writeByte(0)
       ..write(obj.name)
       ..writeByte(1)
-      ..write(obj.age);
+      ..write(obj.age)
+      ..writeByte(3)
+      ..write(obj.pets);
   }
 
   @override
@@ -45,7 +48,7 @@ class UserAdapter extends TypeAdapter<User> {
 
 class PetAdapter extends TypeAdapter<Pet> {
   @override
-  final int typeId = 0;
+  final int typeId = 1;
 
   @override
   Pet read(BinaryReader reader) {
